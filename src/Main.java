19a651877df479dad1c7a3de04839c67ff5b3ac1
Яна Попова <ynappva@gmail.com>
2024-04-22 13:02:46 +0300

    src updated

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         int account = 300;
-        int amount = 500;
-        boolean amountExceeds = true;
+        int amount = 2300;
+        int amountForBonus = 1000;
         int bonus;
         int bonusAmount = amount / 100;
-        int totalAmount = account + amount + bonusAmount;
+        int totalAmount;
 
-
-        if (amountExceeds) {
-            bonus = 1;
+        if (amount > amountForBonus) {
+            //     bonus = 1;
             totalAmount = account + amount + bonusAmount;
-
-            if (amount > 1000) {
-                bonus = 1;
-            } else {
-                bonus = 0;
-            }
-            if (amount < 1000) {
-                bonus = 0;
-                totalAmount = account + amount;
-            }
-
-            System.out.println("Сумма счёта: " + totalAmount);
+        } else {
+            //     bonus = 0;
+            totalAmount = account + amount;
         }
+        System.out.println("Сумма счёта: " + totalAmount);
     }
 }
+
+
